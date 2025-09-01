8122339caa9a84763125bff62ade90b58a4a0e95
ASulthoni <asulthoni.freelancer@gmail.com>
2025-09-01 21:15:59 +0700

    ubah int

---
+++ bin/dasar_dart/08_menukar_nilai_dua_bilangan.dart
@@ -0,0 +1,14 @@
+// menukar nilai 2 bilangan
+
+void main(List<String> args) {
+  int a=5;
+  int b=8;
+
+  a = a+b;
+  b = a-b;
+  a = a-b;
+
+  print('nilai a = $a');
+  print('nilai b = $b');
+
+}
---
+++ bin/dasar_dart/10_conversi_string_to_int.dart
@@ -0,0 +1,6 @@
+void main(List<String> args) {
+  String umur = '20';
+  print('Type umur sekarang adalah : ${umur.runtimeType} ');
+  int number = int.parse(umur);
+  print('Type umur sekarang adalah : ${number.runtimeType}');
+}
